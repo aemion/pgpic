13ee5de78c578b203f2e4a5255ae03291dda3c5e
Alexandre <alexandre.emion@insa-rouen.fr>
2016-02-07 22:51:12 +0100

    Display of meetings

--- app/scripts/controllers/ReunionsCtrl.js
+++ app/scripts/controllers/ReunionsCtrl.js
@@ -9,6 +9,37 @@
  */
 angular.module('pgpicApp')
   .controller('ReunionsCtrl', ['$scope', function ($scope) {
-   
+    $scope.reunions = [
+      {
+        typeReunion: {
+          nom: 'Reunion client'
+        },
+        debut: '2016-02-07T15:04:45', // add hour
+        fin: '2016-02-07T15:48:49', // add hour
+        partiesInteressees: [
+          {
+            nom: 'Emion',
+            prenom: 'Alexandre'
+          },
+          {
+            nom: 'Emion',
+            prenom: 'Thibaut'
+          }
+        ]
+      }
+    ];
+
+    $scope.debut = function(reunion) {
+      return new Date(reunion.debut);
+    };
+
+    $scope.fin = function(reunion) {
+      return new Date(reunion.fin);
+    };
+
+    $scope.duree = function(reunion) {
+      return Math.round((this.fin(reunion) - this.debut(reunion))/60000);
+    };
+
 
   }]);
