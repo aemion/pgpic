b54cdfe5b492a5072991583a87487c9005b9ac52
Alexandre <alexandre.emion@insa-rouen.fr>
2016-02-08 22:26:24 +0100

    Add CR to meetings

--- app/scripts/controllers/ReunionsCtrl.js
+++ app/scripts/controllers/ReunionsCtrl.js
@@ -12,10 +12,29 @@ angular.module('pgpicApp')
     $scope.reunions = [
       {
         typeReunion: {
-          nom: 'Reunion client'
+          nom: 'Reunion tuteur pédagogique',
+          typeCR: null
         },
         debut: '2016-02-07T15:04:45', // add hour
         fin: '2016-02-07T15:48:49', // add hour
+        partiesInteressees: [
+          {
+            nom: 'Emion',
+            prenom: 'Alexandre'
+          },
+          {
+            nom: 'Personne',
+            prenom: 'Autre'
+          }
+        ]
+      },
+      {
+        typeReunion: {
+          nom: 'Reunion client',
+          typeCR: {nom: 'CRC', approbation: true, diffusion: false}
+        },
+        debut: '2016-02-08T17:25:56', // add hour
+        fin: '2016-02-08T17:35:12', // add hour
         partiesInteressees: [
           {
             nom: 'Emion',
@@ -63,5 +82,5 @@ angular.module('pgpicApp')
     };
     $scope.reunion =  {partiesInteressees:[]};
     $scope.partiesInteressees = [{nom: 'Emion', prenom: 'Alexandre'}, {nom: 'Personne', prenom: 'Autre'}];
-    $scope.typesReunions = [{nom: 'Réunion client'}, {nom: 'Réunion tuteur pédagogique'}]; 
+    $scope.typesReunions = [{nom: 'Réunion client', typeCR: {nom: 'CRC', approbation: true, diffusion: false}}, {nom: 'Réunion tuteur pédagogique', typeCR: null}]; 
   }]);
