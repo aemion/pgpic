8a7f9ebe20be8959b450ddcacbaf92498d6d6750
Alexandre <alexandre.emion@insa-rouen.fr>
2016-02-09 17:53:05 +0100

    Move samples meetings to a service

--- app/scripts/controllers/ReunionsCtrl.js
+++ app/scripts/controllers/ReunionsCtrl.js
@@ -8,45 +8,8 @@
  * Controller for the meeting part of pgpicApp
  */
 angular.module('pgpicApp')
-  .controller('ReunionsCtrl', ['$scope', function ($scope) {
-    $scope.reunions = [
-      {
-        typeReunion: {
-          nom: 'Reunion tuteur pédagogique',
-          typeCR: null
-        },
-        debut: '2016-02-07T15:04:45', // add hour
-        fin: '2016-02-07T15:48:49', // add hour
-        partiesInteressees: [
-          {
-            nom: 'Emion',
-            prenom: 'Alexandre'
-          },
-          {
-            nom: 'Personne',
-            prenom: 'Autre'
-          }
-        ]
-      },
-      {
-        typeReunion: {
-          nom: 'Reunion client',
-          typeCR: {nom: 'CRC', approbation: true, diffusion: false}
-        },
-        debut: '2016-02-08T17:25:56', // add hour
-        fin: '2016-02-08T17:35:12', // add hour
-        partiesInteressees: [
-          {
-            nom: 'Emion',
-            prenom: 'Alexandre'
-          },
-          {
-            nom: 'Personne',
-            prenom: 'Autre'
-          }
-        ]
-      }
-    ];
+  .controller('ReunionsCtrl', ['$scope', 'ReunionsService', function ($scope, ReunionsService) {
+    $scope.reunions = ReunionsService.getReunions();
 
     $scope.debut = function(reunion) {
       return new Date(reunion.debut);
---
+++ app/scripts/services/ReunionsService.js
@@ -0,0 +1,49 @@
+'use strict';
+
+angular.module('pgpicApp')
+  .service('ReunionsService', function () {
+    
+    this.reunions = [
+      {
+        typeReunion: {
+          nom: 'Reunion tuteur pédagogique',
+          typeCR: null
+        },
+        debut: '2016-02-07T15:04:45', // add hour
+        fin: '2016-02-07T15:48:49', // add hour
+        partiesInteressees: [
+          {
+            nom: 'Emion',
+            prenom: 'Alexandre'
+          },
+          {
+            nom: 'Personne',
+            prenom: 'Autre'
+          }
+        ]
+      },
+      {
+        typeReunion: {
+          nom: 'Reunion client',
+          typeCR: {nom: 'CRC', approbation: true, diffusion: false}
+        },
+        debut: '2016-02-08T17:25:56', // add hour
+        fin: '2016-02-08T17:35:12', // add hour
+        partiesInteressees: [
+          {
+            nom: 'Emion',
+            prenom: 'Alexandre'
+          },
+          {
+            nom: 'Personne',
+            prenom: 'Autre'
+          }
+        ]
+      }
+    ];
+    
+    this.getReunions = function() {
+      return this.reunions;
+    };
+
+  });
